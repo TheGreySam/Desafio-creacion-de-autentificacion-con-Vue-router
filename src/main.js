a1a5df6e3bb27e59ed0afdf6e8a279894f806274
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-09-28 21:10:08 -0300

    II commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import Firebase from "";
+
+Firebase.initialization({
+  apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_,
+    projectId: process.env.FIREBASE_,
+    storageBucket: process.env.FIREBASE_,
+    messagingSenderId: process.env.FIREBASE_,
+    appId: process.env.FIREBASE_
+
+})
+
+
+
 Vue.config.productionTip = false;
 
 new Vue({
